450d86dac70e648aeb1511e1ea19dcb7a2f6bbea
muammar johnson <muammarj@gmail.com>
2021-02-07 23:51:33 -0600

    added photo, need to attach to recycle view

--- app/src/main/java/com/example/javaproject/view/InfoActivity.java
+++ app/src/main/java/com/example/javaproject/view/InfoActivity.java
@@ -1,15 +1,22 @@
 package com.example.javaproject.view;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.example.javaproject.R;
 import com.example.javaproject.databinding.ActivityInfoBinding;
 import com.example.javaproject.model.Anime;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class InfoActivity extends AppCompatActivity {
     private static final String TAG = "InfoActivity";
@@ -27,11 +34,17 @@ public class InfoActivity extends AppCompatActivity {
         anime =(Anime) intent.getSerializableExtra(chosenAnime);
         Log.d(TAG, "onCreate: "+anime.toString());
         fillInfo();
+
+
     }
 
     private void fillInfo(){
         binding.animeName.setText(anime.getTitle());
         binding.score.setText(String.valueOf(anime.getScore()));
         binding.synopsis.setText(anime.getSynopsis());
+        Glide.with(this).load(anime.getImage_url()).into(binding.animeImage);
+
     }
+
+
 }
